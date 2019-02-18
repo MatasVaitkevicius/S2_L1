07c377a78ba828bec54a0025a504dad5824864cb
Matas Vaitkevičius <vaitkevicius.matas@gmail.com>
2019-02-18 22:15:18 +0200

    Last commit

--- S2_L1/Program.cs
+++ S2_L1/Program.cs
@@ -15,22 +15,26 @@ namespace S2_L1
             const string resultsFile = "U3REZ.TXT"; // Rezultatų failas
 
             var pupilsContainer = ReadPupilsData(friendsNetworkData); // Mokinių konteineris
+            var pupilsContainerCopy2 = pupilsContainer.ShallowCopy();
             var network = ReadNetworkData(networkData); //Kuriuos rysius surasti
             var pupilContainerClone = pupilsContainer.ShallowCopy(); //Mokinių konteinerio kopija
             var alreadyKnowTest = true; // Ar jau pažįstami testas
             var networkList = new List<string>(); // Ryšių sąrašas
             var removedPupil = new Pupil(); // Išimtas moksleivis
             var mutualFriends = new List<string>(); // Bendri draugai
-            PrintToConsole(pupilsContainer, network, alreadyKnowTest, networkList, removedPupil, mutualFriends, friendsNetworkData, networkData);
+            var alreadyBeenPupils = new List<string>(); // Jau buvę draugai
+
+
+            PrintToConsole(pupilsContainer, network, alreadyKnowTest, networkList, removedPupil, mutualFriends, alreadyBeenPupils, friendsNetworkData, networkData);
             Console.WriteLine($"Pradiniai duomenys ir rezultatai spausdinami lentelėmis tekstiniame faile: {resultsFile}");
-            PrintToReportTable(pupilsContainer, network, alreadyKnowTest, networkList, removedPupil, mutualFriends, friendsNetworkData, networkData, resultsFile);
+            PrintToReportTable(pupilsContainer, network, alreadyKnowTest, networkList, removedPupil, mutualFriends, alreadyBeenPupils, friendsNetworkData, networkData, resultsFile);
             Console.ReadKey();
         }
 
         /// <summary>
         /// Nuskaito moksleivių duomenys
         /// </summary>
-        /// <param name="file"> Duomenu failas </param>
+        /// <param name="file"> Duomenų failas </param>
         /// <returns> Moksleivių konteinrį </returns>
         static PupilsContainer ReadPupilsData(string file)
         {
@@ -95,16 +99,15 @@ namespace S2_L1
         /// <param name="removedPupil"> Išimtas moksleivis </param>
         /// <param name="mutualFriends"> Bendri draugai </param>
         /// <returns> Rysio apibudinima </returns>
-        static string NetworkResult(PupilsContainer pupilsContainer, string firstPupil, string secondPupil, bool alreadyKnowTest, List<string> networkList, Pupil removedPupil, List<string> mutualFriends)
+        static string NetworkResult(PupilsContainer pupilsContainer, string firstPupil, string secondPupil, bool alreadyKnowTest, List<string> networkList, Pupil removedPupil, List<string> mutualFriends,
+            List<string> alreadyBeenPupils)
         {
             if (networkList.Count != 0) // Tikrina ar ryšių sąrašas tuščias, jei ne tuščias išima moksleivį, kurį pridėjome praeitą kartą kviesdami metodą
             {
                 networkList.RemoveAt(0);
             }
-
             for (int i = 0; i < pupilsContainer.Count; i++)
             {
-
                 if (pupilsContainer.GetPupil(i).Name == firstPupil)
                 {
                     if (alreadyKnowTest) // Tikrina ar jau pažįstami
@@ -118,26 +121,26 @@ namespace S2_L1
                             alreadyKnowTest = false;
                         }
                     }
-
                     for (int j = 0; j < pupilsContainer.GetPupil(i).Friends.Count; j++)
                     {
-                        if (!(NetworkList(networkList, removedPupil) || pupilsContainer.GetPupil(i).Friends[j] == removedPupil.Name)) // Tikrina ar ryšių sąraše nėra to pačio mokinio
-                        {
-                            networkList.Add(pupilsContainer.GetPupil(i).Friends[j]);
-                        }
-
                         if (FirstNetwork(pupilsContainer, pupilsContainer.GetPupil(i).Friends[j], secondPupil))
                         {
                             return $"Bendri pazistami: {string.Join(", ", mutualFriends)}";
+                        }
+
+                        if (NetworkList(alreadyBeenPupils, pupilsContainer.GetPupil(i).Friends[j])) // Tikrina ar ryšių sąraše nėra to pačio mokinio
+                        {
+                            alreadyBeenPupils.Add(pupilsContainer.GetPupil(i).Name);
+                            networkList.Add(pupilsContainer.GetPupil(i).Friends[j]);
                         }
 
                         if (j + 1 == pupilsContainer.GetPupil(i).Friends.Count) // Tikrina ar baigėsi moksleivio draugai
                         {
                             if (networkList.Count != 0) // Jeigu ryšių sąraše kažkas yra eina į vidu ir kviečia save
                             {
-                                removedPupil = pupilsContainer.GetPupil(i);
+                                alreadyBeenPupils.Add(pupilsContainer.GetPupil(i).Name);
                                 pupilsContainer.RemovePupil(i);
-                                return NetworkResult(pupilsContainer, networkList[0], secondPupil, alreadyKnowTest, networkList, removedPupil, mutualFriends);
+                                return NetworkResult(pupilsContainer, networkList[0], secondPupil, alreadyKnowTest, networkList, removedPupil, mutualFriends, alreadyBeenPupils);
                             }
                         }
                     }
@@ -197,16 +200,16 @@ namespace S2_L1
         /// <param name="networkList"> Ryšių sąrašas </param>
         /// <param name="removedPupil"> Išimtas moksleivis</param>
         /// <returns> Taip arba ne </returns>
-        static bool NetworkList(List<string> networkList, Pupil removedPupil)
+        static bool NetworkList(List<string> alreadyBeenPupils, string givenPupil)
         {
-            for (int i = 0; i < networkList.Count; i++)
+            for (int i = 0; i < alreadyBeenPupils.Count; i++)
             {
-                if (networkList[i] == removedPupil.Name)
+                if (alreadyBeenPupils[i] == givenPupil)
                 {
-                    return true;
+                    return false;
                 }
             }
-            return false;
+            return true;
         }
 
         /// <summary>
@@ -262,7 +265,7 @@ namespace S2_L1
         /// <param name="mutualFriends"> Bendri draugai </param>
         /// <param name="friendsNetworkData"> Moksleivių ryšių duomenų failas </param>
         /// <param name="networkData"> Ieškomų moksleivių ryšių duomenų failas </param>
-        static void PrintToConsole(PupilsContainer pupilsContainer, List<PupilsNetwork> network, bool alreadyKnowTest, List<string> networkList, Pupil removedPupil, List<string> mutualFriends,
+        static void PrintToConsole(PupilsContainer pupilsContainer, List<PupilsNetwork> network, bool alreadyKnowTest, List<string> networkList, Pupil removedPupil, List<string> mutualFriends, List<string> alreadyBeenPupils,
             string friendsNetworkData, string networkData)
         {
             Console.WriteLine("LD_16.Pazintis");
@@ -293,7 +296,7 @@ namespace S2_L1
                 Console.ReadKey();
                 var pupilsContainerCopy = pupilsContainer.ShallowCopy();
                 mutualFriends = MutualFriends(pupilsContainerCopy, pupil.FirstFriend, pupil.SecondFriend);
-                var result = NetworkResult(pupilsContainerCopy, pupil.FirstFriend, pupil.SecondFriend, alreadyKnowTest, networkList, removedPupil, mutualFriends);
+                var result = NetworkResult(pupilsContainerCopy, pupil.FirstFriend, pupil.SecondFriend, alreadyKnowTest, networkList, removedPupil, mutualFriends, alreadyBeenPupils);
                 Console.WriteLine(result);
                 Console.WriteLine();
             }
@@ -312,7 +315,7 @@ namespace S2_L1
         /// <param name="friendsNetworkData"> Moksleivių ryšių duomenų failas </param>
         /// <param name="networkData"> Ieškomų moksleivių ryšių duomenų failas </param>
         /// <param name="resultsFile"> Rezultatų failas </param>
-        static void PrintToReportTable(PupilsContainer pupilsContainer, List<PupilsNetwork> network, bool alreadyKnowTest, List<string> networkList, Pupil removedPupil, List<string> mutualFriends,
+        static void PrintToReportTable(PupilsContainer pupilsContainer, List<PupilsNetwork> network, bool alreadyKnowTest, List<string> networkList, Pupil removedPupil, List<string> mutualFriends, List<string> alreadyBeenPupils,
         string friendsNetworkData, string networkData, string resultsFile)
         {
             using (var writer = new StreamWriter(resultsFile, false, Encoding.UTF8))
@@ -350,8 +353,8 @@ namespace S2_L1
                 {
                     var pupilsContainerCopy = pupilsContainer.ShallowCopy();
                     mutualFriends = MutualFriends(pupilsContainerCopy, friend.FirstFriend, friend.SecondFriend);
-                    var result = NetworkResult(pupilsContainerCopy, friend.FirstFriend, friend.SecondFriend, alreadyKnowTest, networkList, removedPupil, mutualFriends);
-                    writer.WriteLine($"{friend.PrintNetworkToReportTable()} {result, -40} |");
+                    var result = NetworkResult(pupilsContainerCopy, friend.FirstFriend, friend.SecondFriend, alreadyKnowTest, networkList, removedPupil, mutualFriends, alreadyBeenPupils);
+                    writer.WriteLine($"{friend.PrintNetworkToReportTable()} {result,-40} |");
                 }
                 writer.WriteLine(new string('-', 119));
             }
